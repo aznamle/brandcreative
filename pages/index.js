288b77926710c5595feb922b8a36f103b057fa2f
aznamle <namle1998@hotmail.com>
2021-06-28 16:29:34 -0700

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 
 export default function Home() {
   return (
-    <div>
-      test
+    <div className='content-center mx-auto'>
+      <h1 className='text-2xl justify-center'>this is a test</h1>
     </div>
   )
 }
---
+++ prismic.configuration.js
@@ -0,0 +1,37 @@
+import Prismic from '@prismicio/client'
+
+//endpoint
+export const apiEndpoint = 'https://brandcreative.prismic.io/api/v2'
+export const accessToken = ''
+
+// Client method to query documents from the Prismic repo
+export const Client = (req = null) => (
+  Prismic.client(apiEndpoint, createClientOptions(req, accessToken))
+)
+
+const createClientOptions = (req = null, prismicAccessToken = null) => {
+  const reqOption = req ? { req } : {}
+  const accessTokenOption = prismicAccessToken ? { accessToken: prismicAccessToken } : {}
+  return {
+    ...reqOption,
+    ...accessTokenOption,
+  }
+}
+
+export const linkResolver = (doc) => {
+    if (doc.type === 'page') {
+      return `/${doc.uid}`;
+    } else if (doc.type === 'blog') {
+      return `/blog/${doc.uid}`
+    }
+    return '/'
+}
+
+export const hrefResolver = (doc) => {
+  if (doc.type === 'page') {
+    return '/[uid]'
+  } else if (doc.type === 'blog') {
+    return `/blog/[uid]`
+  }
+  return '/'
+}  
