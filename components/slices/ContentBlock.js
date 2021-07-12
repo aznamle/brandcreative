6c182a5723908f1e61bf0a16959e352a6dd624db
aznamle <namle1998@hotmail.com>
2021-07-12 16:11:50 -0700

    added conditional for image alignment

--- components/slices/ContentBlock.js
+++ components/slices/ContentBlock.js
@@ -2,20 +2,37 @@ import { RichText } from 'prismic-reactjs'
 import React from 'react'
 
 const ContentBlock = ({ slice }) => {
+    
+    const align = slice.primary.image_align
+    console.log(slice)
     return (
         <div className='max-w-screen-2xl mx-auto py-32 space-y-6'>
+            
             <div className='max-w-4xl '>
-                <h1 className='font-light text-4xl leading-relaxed'>{RichText.asText(slice.primary.content_block_title)}</h1>
+                <h1 className='font-light text-5xl leading-relaxed'>{RichText.asText(slice.primary.content_block_title)}</h1>
             </div>
+
             <div className='flex space-x-10'>
+
+                { align == false ? 
+                    <div className='w-2/3'>
+                        <img src={slice.primary.content_block_image.url} />
+                    </div>
+                    : undefined
+                }
+
                 <div className='w-1/3 space-y-6'>
                     <h1 className='font-extralight text-4xl leading-relaxed'>{RichText.asText(slice.primary.content_block_subtitle)}</h1>
                     <p className='font-extralight text-xl leading-relaxed'>{RichText.asText(slice.primary.content_block_description)}</p>
                 </div>
                 
-                <div className='w-2/3'>
-                    <img src={slice.primary.content_block_image.url} />
-                </div>
+                { align == true ? 
+                    <div className='w-2/3'>
+                        <img src={slice.primary.content_block_image.url} />
+                    </div>
+                    : undefined
+                }
+
             </div>
         </div>
     )
