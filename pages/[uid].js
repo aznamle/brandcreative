f2ec762a30513c103199c54d51ee0d8cd44e20c6
aznamle <namle1998@hotmail.com>
2021-07-19 16:05:42 -0700

    added clients query for clients block

--- pages/[uid].js
+++ pages/[uid].js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Prismic from '@prismicio/client'
 import { Client } from '../prismic-configuration'
 import { SliceZone } from '../components'
 import { queryRepeatableDocuments } from '../util/queries'
@@ -7,7 +8,8 @@ import Head from 'next/head'
 import { RichText } from 'prismic-reactjs'
 
 
-const Page = ({ doc }) => {
+const Page = ({ doc, clients }) => {
+  console.log(doc)
   if(!doc) return <div>Loading</div>
       if (doc && doc.data) {
       return (
@@ -19,7 +21,7 @@ const Page = ({ doc }) => {
                 content = {RichText.asText(doc.data.meta_description)}
               />
             </Head>
-            <SliceZone sliceZone={doc.data.body} />
+            <SliceZone sliceZone={doc.data.body} clients={clients}/>
           </div>
           
       )
@@ -32,11 +34,19 @@ export async function getStaticProps({ params, preview = null, previewData = {} 
     const { ref } = previewData
     const client = Client();
     const doc = await client.getByUID('page', params.uid, ref ? { ref } : null) || {}
+
+    const clients = await client.query(
+      Prismic.Predicates.at("document.type", "clients"), {
+        orderings: "[my.clients.publish_date desc]",
+        ...(ref ? { ref } : null)
+      },
+    )
   
     return {
       props: {
         preview,
         doc,
+        clients: clients ? clients.results: [],
       },
       revalidate: 1,
     }
