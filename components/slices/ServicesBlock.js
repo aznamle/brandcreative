a864e435e8c9ef9a122b9c006ffd291e60b7d1c1
aznamle <namle1998@hotmail.com>
2021-07-30 15:13:29 -0700

    added images for serviceblock slice

--- components/slices/ServicesBlock.js
+++ components/slices/ServicesBlock.js
@@ -5,13 +5,16 @@ import Section from '../Section'
 import Image from 'next/image'
 
 const ServicesBlock = ({ slice }) => {
+    console.log(slice)
     return (
         <div className='bg-gray-50'>
             <div className='max-w-screen-2xl mx-auto py-36'>
-                <Section>
-                    <div className='grid grid-flow-row grid-cols-3 grid-rows-1 gap-12'>
+                    <div className='grid grid-flow-row grids-col-1 md:grid-cols-3 grid-rows-1 gap-12'>
+
                         {slice.items.map((item,i) => (
-                            <div key={i} className='items-center text-center space-y-2 py-12'>
+                        <Section>
+                            <div key={i} className='items-center text-center space-y-2 py-2 md:py-12'>
+                                <Image src={item.service_icon.url} width={60} height={60} />
                                 <h1 className='text-xl font-bold'>{RichText.asText(item.service_title)}</h1>
                                 <p className='text-gray-400 font-light leading-loose'>
                                     {item.service_description.map(text => (
@@ -21,9 +24,9 @@ const ServicesBlock = ({ slice }) => {
                                     ))}
                                 </p>
                             </div>
+                        </Section>
                         ))}
                     </div> 
-                </Section>
             </div>
         </div>
     )
