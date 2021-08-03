7551bc03f811b791e0831e9d49aa764d3b3fcbeb
aznamle <namle1998@hotmail.com>
2021-08-03 09:42:17 -0700

    changes

--- components/slices/TeamBlock.js
+++ components/slices/TeamBlock.js
@@ -7,13 +7,13 @@ import Image from 'next/image'
 const TeamBlock = ({ slice }) => {
     return (
         <div className='max-w-screen-xl mx-auto py-36'>
-            <div className='grid grid-flow-row grid-cols-4 grid-rows-1 gap-12'>
+            <div className='grid grid-flow-row grid-cols-2 md:grid-cols-4 grid-rows-1 gap-12'>
                 {slice.items.map((item,i) => (
                     <Section key={i}>
-                    <div className='items-center justify-center text-center'>
-                        <Image className='' src={item.employee_image.url} width={item.employee_image.dimensions.width} height={item.employee_image.dimensions.height} />
-                        <h1 className='font-bold text-black text-xl'>{RichText.asText(item.employee_name)}</h1>
-                    </div>
+                        <div className='items-center justify-center text-center'>
+                            <Image className='' src={item.employee_image.url} width={item.employee_image.dimensions.width} height={item.employee_image.dimensions.height} />
+                            <h1 className='font-bold text-black text-xl'>{RichText.asText(item.employee_name)}</h1>
+                        </div>
                     </Section>
                 ))}
             </div>
