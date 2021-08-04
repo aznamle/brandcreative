2f72023c66468f0a322325549e3a73b66892bc4b
aznamle <namle1998@hotmail.com>
2021-08-04 11:25:17 -0700

    fixed steps showing line at the end

--- components/slices/StepsBlock.js
+++ components/slices/StepsBlock.js
@@ -6,16 +6,23 @@ import Section from '../Section'
 import Image from 'next/image'
 
 const StepsBlock = ({ slice }) => {
+    const itemslen = slice.items.length
+
     return (
         <div className="text-gray-600 body-font">
+            <h1 className='text-center text-black font-light text-2xl'>
+                {RichText.asText(slice.primary.steps_block_title)}
+            </h1>
             <div className="max-w-screen-2xl py-24 mx-auto flex flex-wrap">
+
                 <div className="flex flex-wrap w-full">
                     <div className="lg:w-2/5 md:w-1/2 md:pr-16 md:py-6">
 
-                        {slice.items.map((step,items, i ) => (
-                        <div className="flex relative pb-12">
+                        
+                        {slice.items.map((step, i ) => (
+                        <div key={i} className="flex relative pb-12">
                             <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
-                                <div className={`h-full ${ (items[items.length-1] === step) ? 'w-0' : 'w-1' } bg-gray-200 pointer-events-none`}></div>
+                                <div className={`h-full ${ (itemslen === i + 1) ? 'w-0' : 'w-1' } bg-gray-200 pointer-events-none`}></div>
                             </div>
                             <div className="flex-shrink-0 w-10 h-10 rounded-full bg-white inline-flex items-center justify-center text-white relative z-10">
                                 <Image src={step.step_icon.url} width={20} height={20}/>
