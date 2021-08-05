75eea0b65cd04d4e632315a803ff752529209a63
aznamle <namle1998@hotmail.com>
2021-08-05 11:39:07 -0700

    mobile styling adjustment

--- components/slices/ClientsListBlock.js
+++ components/slices/ClientsListBlock.js
@@ -2,29 +2,28 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { RichText } from 'prismic-reactjs'
-import { hrefResolver, linkResolver } from '../../prismic-configuration'
 
 
-const ClientsListBlock = ({ slice, clients }) => {
+const ClientsListBlock = ({ slice }) => {
     const logos = slice.items.length
 
     return (
-        <div className='bg-gray-50'>
+        <div className='bg-gray-100'>
             <div className='py-24 space-y-24'>
-                <div className='md:flex max-w-3xl lg:max-w-5xl mx-auto items-stretch justify-center'>
+                <div className='md:flex max-w-3xl lg:max-w-5xl mx-auto items-stretch justify-center '>
                     <div className='text-black md:w-1/3 text-center'>
                         <h1 className='text-5xl font-bold tracking-wide'>{RichText.asText(slice.primary.client_block_title)}</h1>
                     </div>
-                    <div className='md:w-2/3 text-center'>
+                    <div className='md:w-2/3 text-center px-4'>
                         <p className='text-xl font-extralight tracking-wide leading-relaxed text-gray-700'>{RichText.asText(slice.primary.clients_block_description)}</p>
                     </div>
                 </div>
 
-                <div className={`mx-auto space-x-3 grid ${ logos <= 3 ? `grid-cols-3 max-w-6xl` : `grid-cols-5 max-w-screen-2xl` } `}>
+                <div className={`mx-auto grid px-4 ${ logos <= 3 ? `grid-cols-1 md:grid-cols-3 gap-6 max-w-6xl px-12 md:px-0` : `px-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 max-w-screen-2xl` } `}>
                     {slice.items.map((client, i) => (
                         <div key={i} className='bg-white flex'>
                             <Link href={`clients/${client.client_page.uid}`}>
-                                <Image  className='object-center block transform duration-300 ease-out hover:scale-75' src={client.client_logo.url} width={client.client_logo.dimensions.width} height={client.client_logo.dimensions.height} />
+                                <Image className='object-center block transform duration-300 ease-out hover:scale-75' src={client.client_logo.url} width={client.client_logo.dimensions.width} height={client.client_logo.dimensions.height} />
                             </Link>
                         </div>
                     ))}
--- components/slices/TeamBlock.js
+++ components/slices/TeamBlock.js
@@ -6,21 +6,25 @@ import Image from 'next/image'
 
 const TeamBlock = ({ slice }) => {
     return (
-        <div class="text-gray-600 body-font">
-            <div class="container  py-24 mx-auto">
-                <div class="flex flex-col text-center w-full pb-8">
-                    <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Our Team</h1>
-                </div>
-                <div class="grid grid-flow-row grid-cols-1  md:grid-cols-3 grid-rows-1 gap-2 -m-2">
+        <div className="text-gray-600 body-font">
+            <div className="container py-12 md:py-24 mx-auto">
+                <Section>
+                    <div className="flex flex-col text-center w-full pb-4 md:pb-8">
+                        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Our Team</h1>
+                    </div>
+                </Section>
+                <div className="grid grid-flow-row grid-cols-1  md:grid-cols-3 grid-rows-1 gap-2 px-2">
                 {slice.items.map((item,i) => (
-                    <div class="p-2 w-full">
-                        <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg space-x-6">
+                    <div className="p-2 w-full">
+                    <Section>
+                        <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg space-x-6">
                             <Image className='bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4' src={item.employee_image.url} width={100} height={100} alt='team' />
-                            <div class="flex-grow">
-                                <h2 class="text-gray-900 title-font font-medium">{RichText.asText(item.employee_name)}</h2>
-                                <p class="text-gray-500">UI Designer</p>
+                            <div className="flex-grow">
+                                <h2 className="text-gray-900 title-font font-medium text-sm lg:text-xl">{RichText.asText(item.employee_name)}</h2>
+                                <p className="text-gray-500 text-sm lg:text-lg font-light">{RichText.asText(item.employee_role)}</p>
                             </div>
                         </div>
+                    </Section>
                     </div>
                     ))}
                 </div>
